8e619ce387cb62e30ab5b9748588b71754478127
Mukesh <mukeshprasad121@gmail.com>
2022-10-17 03:18:49 +0530

    Menu bar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './admin/users/users.component';
+import { InsuranceComponent } from './admin/insurance/insurance.component';
 
 const routes: Routes = [
-  // { path: 'admin/users', component: UsersComponent },
+   { path: 'admin/view-insurance', component: InsuranceComponent },
 ];
 
 @NgModule({
